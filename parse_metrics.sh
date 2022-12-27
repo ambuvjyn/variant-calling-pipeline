eb4cc0c22c369083fce0fb766ebb1fa5650e2ffa
Ambu Vijayan <82611533+ambuvjyn@users.noreply.github.com>
2022-12-27 10:57:01 +0530

    Update parse_metrics.sh
    Updating the corrections for the latest version of GATK4

--- parse_metrics.sh
+++ parse_metrics.sh
@@ -9,15 +9,26 @@ do
 	fi
 done < $input
 
+# Match the first line that starts with a number (the stats)
 re='^[0-9]+([.][0-9]+)?$'
 input=${ID}_insert_metrics.txt
 while read line
 do
-	MEAN_INSERT_SIZE=$(echo $line | cut -d' ' -f5)
-	if [[ $MEAN_INSERT_SIZE =~ $re ]];then
+	MEAN_INSERT_SIZE=$(echo $line | cut -d' ' -f6)
+	if [[ $line != '#'* && $MEAN_INSERT_SIZE =~ $re ]];then
         	break
 	fi
 done < $input
+
+input=${ID}_dedup_metrics.txt
+while read line
+do
+        DUP_PCT=$(echo $line | cut -d' ' -f9)
+        if [[ $line != '#'* && $DUP_PCT =~ $re ]];then
+                break
+        fi
+done < $input
+
 
 snps_1=$(grep -v '^#' ${ID}_raw_snps.vcf | wc -l)
 snps_2=$(grep 'PASS' ${ID}_filtered_snps.vcf | wc -l)
@@ -25,5 +36,5 @@ snps_3=$(grep -v '^#' ${ID}_raw_snps_recal.vcf | wc -l)
 snps_4=$(grep 'PASS' ${ID}_filtered_snps_final.vcf | wc -l)
 avg_coverage=$(awk '{sum+=$3} END { print sum/NR}' ${ID}_depth_out.txt)
 
-echo "$ID,$ALIGNMENT_METRICS,$MEAN_INSERT_SIZE,$snps_1,$snps_2,$snps_3,$snps_4,$avg_coverage" >> ../report.csv
-
+echo "ID,# reads,aligned reads,% aligned,aligned bases,read length,% paired, %dup, mean insert size,# SNPs,# filtered SNPs,# SNPs post-bqsr,# SNPs filtered post-bqsr,average coverage"
+echo "$ID,$ALIGNMENT_METRICS,$DUP_PCT,$MEAN_INSERT_SIZE,$snps_1,$snps_2,$snps_3,$snps_4,$avg_coverage"
